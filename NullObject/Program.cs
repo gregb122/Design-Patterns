a8cd8579e25a740fc854b144c53f6738f44c4310
Grzegorz Bielecki <288388@uwr.edu.pl>
2019-04-13 02:20:31 +0200

    Initial commit

---
+++ NullObject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NullObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
